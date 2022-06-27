19de98dc179db9728f53c3e78a704aae711bc277
yeongjuyun <myyun1029@gmail.com>
2022-06-27 22:10:22 +0900

    0627

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
             render={() => (
               <React.Fragment>
                 <AddPostForm />
-                <PostsList />
+                <PostsList />  
               </React.Fragment>
             )}
           />
